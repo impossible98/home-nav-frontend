59340ec730eb4ad11d27008a45627ec6ea346fae
impossible98 <ago2chris@outlook.com>
2022-08-16 21:09:47 +0800

    style: style code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 // import third-party modules
 import React from "react";
-import { Typography } from 'antd';
 // import local modules
 import Content from "../components/home/Content";
 
 function Home() {
   return (
     <div>
-      <Typography.Title>Home</Typography.Title>
       <Content />
     </div>
   );
